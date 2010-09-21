a1413a958b5b995bc36da8277861960133448692
Ronnie Kolehmainen <ronnie.kolehmainen@gmail.com>
2010-09-21 17:34:00 +0000

    Removed dependencies on idea.jar

--- src/com/googlecode/cssxfire/ui/LegendDescriptorPanel.java
+++ src/com/googlecode/cssxfire/ui/LegendDescriptorPanel.java
@@ -16,30 +16,124 @@
 
 package com.googlecode.cssxfire.ui;
 
-import com.intellij.openapi.diff.ex.DiffStatusBar;
-import com.intellij.openapi.editor.colors.EditorColorsScheme;
-
+import javax.swing.*;
 import java.awt.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Ronnie
+ * Shamelessly copied from {@link com.intellij.openapi.diff.ex.DiffStatusBar} since it does not belong to the open-api.
  */
-public class LegendDescriptorPanel extends DiffStatusBar
+public class LegendDescriptorPanel extends JPanel
 {
-    private static final java.util.List<? extends LegendTypeDescriptor> MY_TYPES =
+    private static final java.util.List<MyLegendTypeDescriptor> MY_TYPES =
             Arrays.asList(new MyLegendTypeDescriptor("Modified", Colors.MODIFIED_LEGEND),
                     new MyLegendTypeDescriptor("Added", Colors.ADDED_LEGEND),
                     new MyLegendTypeDescriptor("Invalid", Colors.INVALID_LEGEND));
 
+    private final Collection<JComponent> myLabels = new ArrayList<JComponent>();
+
+    private final JLabel myTextLabel = new JLabel("");
+    private static final int COMP_HEIGHT = 30;
+
     public LegendDescriptorPanel()
     {
-        super(MY_TYPES);
-        setBorder(null);
+        for (MyLegendTypeDescriptor type : MY_TYPES)
+        {
+            addComponent(type);
+        }
+        initGui();
     }
 
-    private static class MyLegendTypeDescriptor implements DiffStatusBar.LegendTypeDescriptor
+    private void addComponent(final MyLegendTypeDescriptor diffType)
+    {
+        JComponent component = new JPanel()
+        {
+            public void paint(Graphics g)
+            {
+                setBackground(UIManager.getColor("Panel.background"));
+                super.paint(g);
+                FontMetrics metrics = getFontMetrics(getFont());
+
+                g.setColor(diffType.getLegendColor());
+                g.fill3DRect(10, (getHeight() - 10) / 2, 35, 10, true);
+
+                Font font = g.getFont();
+                if (font.getStyle() != Font.PLAIN)
+                {
+                    font = font.deriveFont(Font.PLAIN);
+                }
+                g.setFont(font);
+                g.setColor(UIManager.getColor("Label.foreground"));
+                int textBaseline = (getHeight() - metrics.getHeight()) / 2 + metrics.getAscent();
+                g.drawString(diffType.getDisplayName(), 67, textBaseline);
+            }
+
+            @Override
+            public Dimension getPreferredSize()
+            {
+                FontMetrics metrics = getFontMetrics(getFont());
+                return new Dimension((int) (70 + metrics.getStringBounds(diffType.getDisplayName(), getGraphics()).getWidth()), COMP_HEIGHT);
+            }
+
+            @Override
+            public Dimension getMinimumSize()
+            {
+                return getPreferredSize();
+            }
+        };
+        myLabels.add(component);
+    }
+
+    public Dimension getMinimumSize()
+    {
+        Dimension p = super.getPreferredSize();
+        Dimension m = super.getMinimumSize();
+        return new Dimension(m.width, p.height);
+    }
+
+    public Dimension getMaximumSize()
+    {
+        Dimension p = super.getPreferredSize();
+        Dimension m = super.getMaximumSize();
+        return new Dimension(m.width, p.height);
+    }
+
+    public void setText(String text)
+    {
+        myTextLabel.setText(text);
+    }
+
+    private void initGui()
+    {
+        JComponent filler = new JComponent()
+        {
+            @Override
+            public Dimension getPreferredSize()
+            {
+                return myTextLabel.getPreferredSize();
+            }
+        };
+        setLayout(new BorderLayout());
+
+        add(myTextLabel, BorderLayout.WEST);
+        Box box = Box.createHorizontalBox();
+        box.add(Box.createHorizontalGlue());
+        JPanel panel = new JPanel(new GridLayout(1, myLabels.size(), 0, 0));
+        for (final JComponent myLabel : myLabels)
+        {
+            panel.add(myLabel);
+        }
+        panel.setMaximumSize(panel.getPreferredSize());
+        box.add(panel);
+        box.add(Box.createHorizontalGlue());
+        add(box, BorderLayout.CENTER);
+
+        add(filler, BorderLayout.EAST);
+    }
+
+    private static class MyLegendTypeDescriptor
     {
         private final String displayName;
         private final Color legendColor;
@@ -55,7 +149,7 @@ public class LegendDescriptorPanel extends DiffStatusBar
             return displayName;
         }
 
-        public Color getLegendColor(EditorColorsScheme colorScheme)
+        public Color getLegendColor()
         {
             return legendColor;
         }
